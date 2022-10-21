72870b260782ece8ff2a443159e593a4c58aa703
jonatasas10 <jonatasas17@gmail.com>
2022-10-20 22:42:45 -0300

    main.c

--- main.c
+++ main.c
@@ -25,9 +25,10 @@ typedef struct afd{
 void visualizacao(afd *saida){
     FILE* arquivo = fopen("/home/jonatas/Jonatas/VSCODE/FTC/afd.txt","w");
     fprintf(arquivo, "digraph finite_state_machine {\n");
-    fprintf(arquivo, "fontname=\"Helvetica,Arial,sans-serif\"\n");
-    fprintf(arquivo, "node [fontname=\"Helvetica,Arial,sans-serif\"]\n");
-    fprintf(arquivo, "edge [fontname=\"Helvetica,Arial,sans-serif\"]\n");
+    fprintf(arquivo, "\tfontname=\"Helvetica,Arial,sans-serif\"\n");
+    fprintf(arquivo, "\tnode [fontname=\"Helvetica,Arial,sans-serif\"]\n");
+    fprintf(arquivo, "\tedge [fontname=\"Helvetica,Arial,sans-serif\"]\n");
+    fprintf(arquivo, "\tnode [shape = point ]; qi");
     fprintf(arquivo, "\trankdir=LR;\n");
 
     fprintf(arquivo, "\tnode [shape = doublecircle, width=.75, height=.5];");
@@ -36,6 +37,7 @@ void visualizacao(afd *saida){
     }
     fprintf(arquivo,";\n");
     fprintf(arquivo, "\tnode [shape = circle,width=.75, height=.05];\n");
+    fprintf(arquivo, "\tqi -> %s;\n", saida->estado_inicial);
     for (int i = 0; i < saida->qtd_transicoes; i++){
         fprintf(arquivo,"\t%s -> %s [label = \"%s\"];\n", saida->transicao[i].origem, saida->transicao[i].destino,saida->transicao[i].ler);
     }
@@ -47,13 +49,12 @@ void visualizacao(afd *saida){
 
 
 void cria_AFD(afd *entrada, FILE *afd_arq){
-    entrada->qtd_estados = 2;
-    int x;
-    //fscanf(afd_arq, "%d", &x);
-   //entrada->estado = calloc(sizeof(*entrada->estado),entrada->qtd_estados);
 
-    printf("ESTADO: %d\n", entrada->qtd_estados);
-    /*
+    fscanf(afd_arq, "%d", &entrada->qtd_estados);
+   entrada->estado = calloc(sizeof(*entrada->estado),entrada->qtd_estados);
+
+    //printf("ESTADO: %d\n", entrada->qtd_estados);
+    
     for (int i = 0; i < entrada->qtd_estados; i++){
         entrada->estado[i] = calloc(sizeof(entrada->estado),20);
         fscanf(afd_arq, "%s", entrada->estado[i]);
@@ -61,21 +62,21 @@ void cria_AFD(afd *entrada, FILE *afd_arq){
 
     fscanf(afd_arq, "%d", &entrada->qtd_alfabeto);
     entrada->alfabeto = calloc(sizeof(*entrada->alfabeto),entrada->qtd_alfabeto);
-    printf("ALFABETO:\n");
+    //printf("ALFABETO:\n");
     for (int i = 0; i < entrada->qtd_alfabeto; i++){
         entrada->alfabeto[i] = calloc(sizeof(entrada->alfabeto), 1);
         fscanf(afd_arq, "%s", entrada->alfabeto[i]);
     }
     
     fscanf(afd_arq, "%d", &entrada->qtd_transicoes);
-   printf("TRANSICOES:\n");
+   //printf("TRANSICOES:\n");
    entrada->transicao = calloc(sizeof(transicao), entrada->qtd_transicoes);
     for (int i = 0; i < entrada->qtd_transicoes; i++){
         fscanf(afd_arq, "%s%s%s", entrada->transicao[i].origem, entrada->transicao[i].ler, entrada->transicao[i].destino);
         
     } 
     
-    printf("ESTADO INICIAL:\n");
+    //printf("ESTADO INICIAL:\n");
     entrada->estado_inicial = calloc(sizeof(entrada->estado_inicial),20);
     fscanf(afd_arq, "%s", entrada->estado_inicial);
 
@@ -86,18 +87,45 @@ void cria_AFD(afd *entrada, FILE *afd_arq){
         entrada->estados_finais[i] = calloc(sizeof(entrada->estados_finais[i]),20);
         fscanf(afd_arq, "%s", entrada->estados_finais[i]);
     }
-    */
+    
+    
+}
+
+void complemento (afd *afd){
+    int diferenca = afd->qtd_estados - afd->qtd_estados_finais;
+    afd->estados_finais = realloc(afd->estados_finais, (diferenca+1)*sizeof(*afd->estados_finais));
+    char **estados_finais_c;
+    /*char **estados_finais_c = calloc(sizeof(char*),diferenca);
+    for (int i = 0; i < diferenca; i++){
+        estados_finais_c[i] = calloc(sizeof(char), 20);
+    }*/
+    int contador = 0;
+
+    printf("DIF %d QTD %d FINAL %d\n",diferenca, afd->qtd_estados, afd->qtd_estados_finais);
+    int x = afd->qtd_estados_finais;
+    for (int i = 0; i < x; i++){
+        for (int j = 0; j < afd->qtd_estados; j++){
+            if (strcmp(afd->estados_finais[0], afd->estado[j]) != 0){
+                afd->estados_finais[contador] = afd->estado[j];
+                printf("I = %d TR %s - %s\n", i, afd->estados_finais[i],afd->estado[j]);
+                contador++;
+                //afd->estados_finais = realloc(afd->estados_finais, (contador+1)*sizeof(*afd->estados_finais));
+            }
+        }
+        afd->qtd_estados_finais = diferenca;
+    }
+    printf("DIFERENCA: %d\n", afd->qtd_estados_finais);
     
 }
 
 int main(){
-    FILE *AFD_entrada = fopen("AFD.txt", "r");
+    FILE *AFD_entrada = fopen("/home/jonatas/Jonatas/VSCODE/FTC/AFD2.txt", "r");
     afd *nome = calloc(1,sizeof(afd));
     cria_AFD(nome, AFD_entrada);
-    //visualizacao(nome);
-    printf("RESULTADO\n");
+    //complemento(nome);
+    visualizacao(nome);
     
-   /* for (int i = 0; i < 2; i++){
+    /*for (int i = 0; i < nome->qtd_transicoes; i++){
         printf("%s\n", nome->transicao[i].origem);
          printf("%s\n", nome->transicao[i].ler);
           printf("%s\n", nome->transicao[i].destino);
