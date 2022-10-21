fe4d872ce0d3f09a6a70f1788b12efca63e15a14
jonatasas10 <jonatasas17@gmail.com>
2022-10-20 22:40:41 -0300

    main.c

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ void visualizacao(afd *saida){
     fprintf(arquivo, "edge [fontname=\"Helvetica,Arial,sans-serif\"]\n");
     fprintf(arquivo, "\trankdir=LR;\n");
 
-    fprintf(arquivo, "\tnode [shape = doublecircle, width=.75, height=.5]\;");
+    fprintf(arquivo, "\tnode [shape = doublecircle, width=.75, height=.5];");
     for (int i = 0; i < saida->qtd_estados_finais; i++){
         fprintf(arquivo, "%s ", saida->estados_finais[i]);
     }
@@ -46,61 +46,62 @@ void visualizacao(afd *saida){
 }
 
 
-void cria_AFD(afd *entrada){
+void cria_AFD(afd *entrada, FILE *afd_arq){
     entrada->qtd_estados = 2;
-    entrada->estado = calloc(sizeof(*entrada->estado),2);
+    int x;
+    //fscanf(afd_arq, "%d", &x);
+   //entrada->estado = calloc(sizeof(*entrada->estado),entrada->qtd_estados);
 
-    printf("ESTADO:\n");
+    printf("ESTADO: %d\n", entrada->qtd_estados);
+    /*
     for (int i = 0; i < entrada->qtd_estados; i++){
         entrada->estado[i] = calloc(sizeof(entrada->estado),20);
-        scanf("%s", entrada->estado[i]);
-        fflush(stdin);
+        fscanf(afd_arq, "%s", entrada->estado[i]);
     }
 
-    entrada->qtd_alfabeto = 2;
+    fscanf(afd_arq, "%d", &entrada->qtd_alfabeto);
     entrada->alfabeto = calloc(sizeof(*entrada->alfabeto),entrada->qtd_alfabeto);
     printf("ALFABETO:\n");
     for (int i = 0; i < entrada->qtd_alfabeto; i++){
         entrada->alfabeto[i] = calloc(sizeof(entrada->alfabeto), 1);
-        scanf("%s", entrada->alfabeto[i]);
+        fscanf(afd_arq, "%s", entrada->alfabeto[i]);
     }
     
-    entrada->qtd_transicoes = 2;
+    fscanf(afd_arq, "%d", &entrada->qtd_transicoes);
    printf("TRANSICOES:\n");
    entrada->transicao = calloc(sizeof(transicao), entrada->qtd_transicoes);
     for (int i = 0; i < entrada->qtd_transicoes; i++){
-        scanf("%s", entrada->transicao[i].origem);
-        scanf("%s", entrada->transicao[i].ler);
-        scanf("%s", entrada->transicao[i].destino);  
-        fflush(stdin);         
-    }
+        fscanf(afd_arq, "%s%s%s", entrada->transicao[i].origem, entrada->transicao[i].ler, entrada->transicao[i].destino);
+        
+    } 
     
     printf("ESTADO INICIAL:\n");
     entrada->estado_inicial = calloc(sizeof(entrada->estado_inicial),20);
-    scanf("%s", entrada->estado_inicial);
+    fscanf(afd_arq, "%s", entrada->estado_inicial);
 
-    entrada->qtd_estados_finais = 1;
+    fscanf(afd_arq, "%d", &entrada->qtd_estados_finais);
     entrada->estados_finais = calloc(sizeof(*entrada->estados_finais),entrada->qtd_estados_finais);
     
     for (int i = 0; i < entrada->qtd_estados_finais; i++){
         entrada->estados_finais[i] = calloc(sizeof(entrada->estados_finais[i]),20);
-        scanf("%s", entrada->estados_finais[i]);
-        fflush(stdin);
+        fscanf(afd_arq, "%s", entrada->estados_finais[i]);
     }
+    */
     
 }
 
 int main(){
-    
+    FILE *AFD_entrada = fopen("AFD.txt", "r");
     afd *nome = calloc(1,sizeof(afd));
-    cria_AFD(nome);
-    visualizacao(nome);
+    cria_AFD(nome, AFD_entrada);
+    //visualizacao(nome);
     printf("RESULTADO\n");
     
-    for (int i = 0; i < 2; i++){
+   /* for (int i = 0; i < 2; i++){
         printf("%s\n", nome->transicao[i].origem);
          printf("%s\n", nome->transicao[i].ler);
           printf("%s\n", nome->transicao[i].destino);
-    }
+    }*/
+    fclose(AFD_entrada);
     return 0;
 }
